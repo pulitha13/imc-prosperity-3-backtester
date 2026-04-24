19ca026680ca6e3c6bbece6526e65bb0c1c97a79
Pranav Mantri <pranumantri@gmail.com>
2026-04-24 08:57:46 -0400

    r3 data push

--- prosperity4/data.py
+++ prosperity4/data.py
@@ -13,6 +13,20 @@ ROUND_LIMITS = {
         "ASH_COATED_OSMIUM": 80,
         "INTARIAN_PEPPER_ROOT": 80,
     },
+    3: {
+        "HYDROGEL_PACK": 200,
+        "VELVETFRUIT_EXTRACT": 200,
+        "VEV_4000": 300,
+        "VEV_4500": 300,
+        "VEV_5000": 300,
+        "VEV_5100": 300,
+        "VEV_5200": 300,
+        "VEV_5300": 300,
+        "VEV_5400": 300,
+        "VEV_5500": 300,
+        "VEV_6000": 300,
+        "VEV_6500": 300,
+    },
 }
 
 
