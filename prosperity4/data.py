59a834690a694390670807b3c112463f6c0ebdfb
Pranav Mantri <pranumantri@gmail.com>
2026-04-28 10:25:17 -0400

    round 5 update

--- prosperity4/data.py
+++ prosperity4/data.py
@@ -41,6 +41,67 @@ ROUND_LIMITS = {
         "VEV_6000": 300,
         "VEV_6500": 300,
     },
+    5: {
+            "GALAXY_SOUNDS_DARK_MATTER": 10,
+            "GALAXY_SOUNDS_BLACK_HOLES": 10,
+            "GALAXY_SOUNDS_PLANETARY_RINGS": 10,
+            "GALAXY_SOUNDS_SOLAR_WINDS": 10,
+            "GALAXY_SOUNDS_SOLAR_FLAMES": 10,
+
+            "SLEEP_POD_SUEDE": 10,
+            "SLEEP_POD_LAMB_WOOL": 10,
+            "SLEEP_POD_POLYESTER": 10,
+            "SLEEP_POD_NYLON": 10,
+            "SLEEP_POD_COTTON": 10,
+
+            "MICROCHIP_CIRCLE": 10,
+            "MICROCHIP_OVAL": 10,
+            "MICROCHIP_SQUARE": 10,
+            "MICROCHIP_RECTANGLE": 10,
+            "MICROCHIP_TRIANGLE": 10,
+
+            "PEBBLES_XS": 10,
+            "PEBBLES_S": 10,
+            "PEBBLES_M": 10,
+            "PEBBLES_L": 10,
+            "PEBBLES_XL": 10,
+
+            "ROBOT_VACUUMING": 10,
+            "ROBOT_MOPPING": 10,
+            "ROBOT_DISHES": 10,
+            "ROBOT_LAUNDRY": 10,
+            "ROBOT_IRONING": 10,
+
+            "UV_VISOR_YELLOW": 10,
+            "UV_VISOR_AMBER": 10,
+            "UV_VISOR_ORANGE": 10,
+            "UV_VISOR_RED": 10,
+            "UV_VISOR_MAGENTA": 10,
+
+            "TRANSLATOR_SPACE_GRAY": 10,
+            "TRANSLATOR_ASTRO_BLACK": 10,
+            "TRANSLATOR_ECLIPSE_CHARCOAL": 10,
+            "TRANSLATOR_GRAPHITE_MIST": 10,
+            "TRANSLATOR_VOID_BLUE": 10,
+
+            "PANEL_1X2": 10,
+            "PANEL_2X2": 10,
+            "PANEL_1X4": 10,
+            "PANEL_2X4": 10,
+            "PANEL_4X4": 10,
+
+            "OXYGEN_SHAKE_MORNING_BREATH": 10,
+            "OXYGEN_SHAKE_EVENING_BREATH": 10,
+            "OXYGEN_SHAKE_MINT": 10,
+            "OXYGEN_SHAKE_CHOCOLATE": 10,
+            "OXYGEN_SHAKE_GARLIC": 10,
+
+            "SNACKPACK_CHOCOLATE": 10,
+            "SNACKPACK_VANILLA": 10,
+            "SNACKPACK_PISTACHIO": 10,
+            "SNACKPACK_STRAWBERRY": 10,
+            "SNACKPACK_RASPBERRY": 10,
+        }
 }
 
 
